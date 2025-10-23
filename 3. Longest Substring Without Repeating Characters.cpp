56cf8e2d6a557af79cc2a1dd388899dab28b9103
AKRAMI-AK <akrami.2019.04@gmail.com>
2025-10-23 20:27:11 +0430

    Add files via upload
    I am trying to solve LeetCode Problems!

---
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> lastIndex;
+        int maxLength = 0;
+        int start = 0;
+
+        for( int end = 0; end < s.size(); end++){
+            char c = s[end];
+
+            if(lastIndex.count(c) && lastIndex[c] >= start){
+                start = lastIndex[c] + 1;
+            }
+
+            lastIndex[c] = end;
+
+            maxLength = max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+};
